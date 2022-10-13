20516ceb65144c179162a92fe5d76d0194c4222d
iOwenDon <114987877+iOwenDon@users.noreply.github.com>
2022-10-13 09:54:17 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<title>
+
+</title>
+<body>
+    
+<h1>
+     
+    <?php
+        print "Hello world ";
+        print date("d M y");
+//add text box at the world part that has my name in it with the date next to it - make it so it can add it to database 
+
+    ?>  
+</h1>
+
+</body>
+</html>
