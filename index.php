610e206deab80880376e18fd4b9ef29570a37411
iOwenDon <114987877+iOwenDon@users.noreply.github.com>
2022-10-14 11:50:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,9 +11,14 @@
         print "Hello world ";
         print date("d M y");
 //add text box at the world part that has my name in it with the date next to it - make it so it can add it to database 
-
     ?>  
 </h1>
+
+<p> Contact details </p>
+<form class="customer details" action="contactform.php" method=post
+inputmode="text" name="name" placeholder="First Name"
+inputmode="text" name="name" placeholder="Second Name"
+inputmode="text" name="name" placeholder="Company"
 
 </body>
 </html>
