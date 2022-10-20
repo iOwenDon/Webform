d5eb6796478f6ff34ad01a1025e29672ac5922b8
iOwenDon <114987877+iOwenDon@users.noreply.github.com>
2022-10-20 11:23:54 +0100

    adding company to post

--- thankyou.php
+++ thankyou.php
@@ -11,12 +11,17 @@
     
     Thank You 
     <?php print $_POST["FirstName"]; ?> 
-    <?php print $_POST["Surname"]; ?>! 
-               
+    <?php print $_POST["Surname"]; ?>!<br>
+
+</h1>
+
+<h2>
+    <?php print "Company:" ?> 
+    <?php print $_POST["Company"]; ?> 
+</h2>              
     
     
 
       
     
     
-</h1>
