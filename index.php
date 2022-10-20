850bc41d998f1250842170fc8949dae97c83e21b
iOwenDon <114987877+iOwenDon@users.noreply.github.com>
2022-10-20 11:14:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -27,57 +27,6 @@ Company <input type="text" name="Company"><br>
 
 
 
-
-
-<?php
-
-// SQL
-
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "CustomerContacts";
-
-// Create Connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check Connection
-if ($conn->connect_error) {
-    die("connection failed:" . $conn->connect_error);
-}
-echo "Connect Successfully"
-
-//Create Database
-$sql = "CREATE DATABASE myDB";
-if ($conn->query($sql) === TRUE) {
-    ECHO "Database Created Successfully";
-} else {
-    echo "Error Creating Database" . $conn->error;
-}
-
-
-$conn->close();
-
-//sql to create table
-$sql= "CREATE TABLE MyCustomers (
-    id TNT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    firstname VARCJAR(30) NOT NULL,
-    lastname VARCHAR(30) NOT NULL,
-    company name VARCHAR(50),
-    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    
-)";
-
-if ($conn->query($sql) === TRUE){
-    ECHO "Table MyCustomers created successfully";
-} else {
-    echo "Error creating table:" . $conn->error;
-}
-
-$conn->close();
-
-?>
-
 </form>
 
 
